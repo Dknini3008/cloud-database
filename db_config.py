c92b5492a2ca9b8d0a733edc46085faf721ca618
Brayan Decanini <a6519150009@utch.edu.mx>
2021-06-02 09:38:12 -0600

    26052021

--- db_config.py
+++ db_config.py
@@ -6,5 +6,7 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 
-client = pymongo.MongoClient(f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@db-example.faqu6.mongodb.net/{DB_NAME}?retryWrites=true&w=majority")
-db = client.test
+client = pymongo.MongoClient(f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@example.faqu6.mongodb.net/{DB_NAME}?retryWrites=true&w=majority")
+
+#Add the database name
+db = client.db_example
